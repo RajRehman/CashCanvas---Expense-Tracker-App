e4cb9eac99e7f55117c11e96579da8d28a8d2d16
rajrehman <rajrehman0205@gmail.com>
2024-03-26 19:35:47 -0400

    Heroku related change in base URI apislice

--- clientfe/src/store/apiSlice.js
+++ clientfe/src/store/apiSlice.js
@@ -1,6 +1,6 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-const baseURI = 'http://localhost:8080';
+const baseURI = 'https://secure-reaches-58010-009d965c3ff3.herokuapp.com/'; //before heroku: http://localhost:8080
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({baseUrl: baseURI}),
